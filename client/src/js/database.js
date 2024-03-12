3979472b7bec8578121a940c224d303e24077816
AndrueGage <trainer.andrue@gmail.com>
2024-03-12 16:09:44 -0500

    added logic to putDB and getDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,25 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Added logic to a method that accepts some content and adds it to the database
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1); 
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const results = await store.put({id: 1, value: content});
+  console.log('Data saved to the DB', results);
+};
+
+// Added logic for a method that gets all the content from the database
+
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const result = await store.getAll();
+  console.log('Data retrieved from DB', result)
+  return result;
+};
 
 initdb();
